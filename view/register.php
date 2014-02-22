26db379a8d5a32449f86c38d53db4d4f30282fdd
kentucky <knt92@hotmail.fr>
2014-02-22 16:58:44 +0100

    Initial commit

--- view/register.php
+++ view/register.php
@@ -172,6 +172,9 @@ require_once '../controller/functions.php';
                 //notre expression regulière pour la verification de la validité du mail
                 var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 
+                //notre expression regulière pour la verification du mot de passe (longueur, MAJ, min, special chars)
+                var passwordReg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@*#]).([a-zA-Z0-9@*#]{8,26})$/;
+
                 var hasError = false;
 
                 //cache le span
@@ -181,55 +184,105 @@ require_once '../controller/functions.php';
                 if (nameVal == '') 
                 {   
 
-                    //insere notre message d'erreur apres la div du password
+                    //insere notre message d'erreur apres la div du name
                     $("#name").after('<span class="error">Veuillez rentrer un nom.</span>');
                     //erreur = 1
                     hasError = true;
-                } 
+                }
+
+                //Sinon si name est <= 2
+                else if ( nameVal.length <= 2 )
+                {
+
+                    //insere notre message d'erreur apres la div du name
+                    $("#name").after('<span class="error">Votre nom est trop court.</span>');
+                    //erreur = 1
+                    hasError = true;
+                }
+
+                //Sinon si name est > 15
+                else if ( nameVal.length > 15 )
+                {
+
+                    //insere notre message d'erreur apres la div du name
+                    $("#name").after('<span class="error">Votre nom est trop long.</span>');
+                    //erreur = 1
+                    hasError = true;
+                }
+
                 //Sinon si firstname est vide
                 else if (firstnameVal == '') 
                 {
 
-                    //insere notre message d'erreur apres la div du passwordConfirmation
+                    //insere notre message d'erreur apres la div du firstname
                     $("#firstname").after('<span class="error">Veuillez rentrer un prenom.</span>');
                     //erreur = 1
                     hasError = true;
-                } 
+                }
+
+                //Sinon si firstname est <= 2
+                else if (firstnameVal.length <= 2)
+                {
+
+                    //insere notre message d'erreur apres la div du firstname
+                    $("#firstname").after('<span class="error">Votre prénom est trop court.</span>');
+                    //erreur = 1
+                    hasError = true;
+                }
+                //Sinon si firstname est > 15
+                else if (firstnameVal.length > 15)
+                {
+
+                    //insere notre message d'erreur apres la div du firstname
+                    $("#firstname").after('<span class="error">Votre prénom est trop long.</span>');
+                    //erreur = 1
+                    hasError = true;
+                }
+
                 //Sinon si email est vide
                 else if (emailVal == '') 
                 {
-                    //insere notre message d'erreur apres la div du passwordConfirmation
+                    //insere notre message d'erreur apres la div du email
                     $("#email").after('<span class="error">Veuillez rentrer un email.</span>');
+                    //erreur = 1
+                    hasError = true;
+                }
+                //verifie la longueur de l'email, email doit etre < 26 sinon erreur
+                else if (emailVal.length > 26)
+                {
+                    //insere notre message d'erreur apres la div du email
+                    $("#email").after('<span class="error">Votre email est trop long.</span>');
                     //erreur = 1
                     hasError = true;
                 }
                 //Test de la validité du mail
                 else if(!emailReg.test(emailVal)) 
                 {
+                    //insere notre message d'erreur apres la div du email
                     $("#email").after("<span class='error'>Votre email n'est pas valide.</span>");
                     hasError = true;
                 }
+
                //Sinon si password est vide
                 else if (passwordVal == '' )
                 {  
-                    //insere notre message d'erreur apres la div du passwordConfirmation pour la non correspondance
+                    //insere notre message d'erreur apres la div du password pour la non correspondance
                     $("#password").after('<span class="error">Veuillez rentrer un mot de passe.</span>');
                     //erreur = 1
                     hasError = true;
                 }
-               
-                //Verification de la correspondance des mdp
+                //Verifie que le mot de passe ne soit pas égal au nom
                 else if (passwordVal == nameVal )
                 { 
-                    //insere notre message d'erreur apres la div du passwordConfirmation pour la non correspondance
+                    //insere notre message d'erreur apres la div du password pour la non correspondance
                     $("#password").after('<span class="error">Votre mot de passe doit être different de votre nom.</span>');
                     //erreur = 1
                     hasError = true;
                 }
-                 //Verification de la correspondance des mdp
+                 //Verifie que le mot de passe ne soit pas égal au prénom
                 else if (passwordVal == firstnameVal )
                 { 
-                    //insere notre message d'erreur apres la div du passwordConfirmation pour la non correspondance
+                    //insere notre message d'erreur apres la div du password pour la non correspondance
                     $("#password").after('<span class="error">Votre mot de passe doit être different de votre prenom.</span>');
                     //erreur = 1
                     hasError = true;
@@ -237,12 +290,20 @@ require_once '../controller/functions.php';
                 //verifie que le mot de passe ne soit pas égal à l'email
                 else if (passwordVal == emailVal )
                 { 
-                    //insere notre message d'erreur apres la div du passwordConfirmation pour la non correspondance
+                    //insere notre message d'erreur apres la div du password pour la non correspondance
                     $("#password").after('<span class="error">Votre mot de passe doit être different de votre email.</span>');
                     //erreur = 1
                     hasError = true;
                 }
-                 //Verification de la correspondance des mdp
+                //verifie que le mot de passe respecte nos conditions (MAJ,min,special char, compris entre 8 et 26 char)
+                else if (!passwordReg.test(passwordVal))
+                {
+                    //insere notre message d'erreur apres la div du password pour la non correspondance
+                    $("#password").after('<span class="error">Votre mot de passe doit contenir 1Majuscule, 1min, 1carac spé, compris entre 8 et 26char.</span>');
+                    //erreur = 1
+                    hasError = true;
+                }
+                //Enfin nous verifierons la correspondance des mdp
                 else if (passwordVal != passwordCheckVal )
                 {
 
