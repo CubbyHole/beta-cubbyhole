1ef09b361fb775acae5c069f4e8eef36f5a8d991
Alban <125637@supinfo.com>
2014-03-08 16:10:11 +0100

    Modification des interfaces #1 Tentative de commit du template d'email d'inscription HTML.

--- model/Interfaces/AccountManager.interface.php
+++ model/Interfaces/AccountManager.interface.php
@@ -1,9 +1,37 @@
 <?php
-interface AccountManager {
-	
-	public function findById($id, $source);
-	
+/**
+ * Created by Notepad++.
+ * User: Alban Truc
+ * Date: 31/01/14
+ * Time: 12:52
+ */
+interface AccountManagerInterface
+{
+    /**
+     * @author Alban Truc
+     * @param $id String|MongoId
+     * @since 01/2014
+     * @return mixed
+     */
+
+    function findById($id);
+
+    /**
+     * @author Alban Truc
+     * @param $account array
+     * @since 02/2014
+     * @return mixed
+     */
+
+    function createAccount($account);
+
+    /**
+     * @author Alban Truc
+     * @param $id String|MongoID
+     * @since 23/02/2014
+     * @return array|bool TRUE si suppression réussie
+     */
+
+    function removeAccount($id);
 }
-
-
 ?>
