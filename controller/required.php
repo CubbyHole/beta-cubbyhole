fb9c756e7a4d2c1720fcd090128659e8863d55c9
CubbyHole <CubbyHole@users.noreply.github.com>
2014-03-13 14:18:43 +0100

    fichier déplacé à la racine

--- controller/required.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '..\model\classes\User.class.php';
-require_once '..\model\classes\RefPlan.class.php';
-require_once '..\model\classes\Account.class.php';
-
-require_once '..\model\pdo\UserPdoManager.class.php';
-require_once '..\model\pdo\RefPlanPdoManager.class.php';
-require_once '..\model\pdo\AccountPdoManager.class.php';
-?>
