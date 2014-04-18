d76ed31e8e71be6b6bb4f306a5c863667b3c7991
Alban <125637@supinfo.com>
2014-04-18 16:53:11 +0200

    inclusion de nouveaux fichiers

--- required.php
+++ required.php
@@ -4,10 +4,21 @@ $projectRoot = $_SERVER['DOCUMENT_ROOT'].'/Cubbyhole';
 require_once $projectRoot.'/model/classes/User.class.php';
 require_once $projectRoot.'/model/classes/RefPlan.class.php';
 require_once $projectRoot.'/model/classes/Account.class.php';
+require_once $projectRoot.'/model/classes/RefAction.class.php';
+require_once $projectRoot.'/model/classes/Transaction.class.php';
 
+require_once $projectRoot.'/model/interfaces/AccountManager.interface.php';
+require_once $projectRoot.'/model/interfaces/RefPlanManager.interface.php';
+require_once $projectRoot.'/model/interfaces/UserManager.interface.php';
+require_once $projectRoot.'/model/interfaces/RefActionManager.interface.php';
+require_once $projectRoot.'/model/interfaces/TransactionManager.interface.php';
+
+require_once $projectRoot.'/model/pdo/AbstractPdoManager.class.php';
 require_once $projectRoot.'/model/pdo/UserPdoManager.class.php';
 require_once $projectRoot.'/model/pdo/RefPlanPdoManager.class.php';
 require_once $projectRoot.'/model/pdo/AccountPdoManager.class.php';
+require_once $projectRoot.'/model/pdo/RefActionPdoManager.class.php';
+require_once $projectRoot.'/model/pdo/TransactionPdoManager.class.php';
 
 require_once $projectRoot.'/controller/functions.php';
 ?>
