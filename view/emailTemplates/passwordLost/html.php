12adb67e8a1d60f76f39043100463155dcf644e5
kentucky <knt92@hotmail.fr>
2014-05-01 14:22:59 +0200

    Ajout du background aqua pour la page

--- view/emailTemplates/passwordLost/html.php
+++ view/emailTemplates/passwordLost/html.php
@@ -5,6 +5,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Activate your Cubbyhole account now</title>
+<link rel="shortcut icon" href="/Cubbyhole/content/img/icons/icons.png">
 
 <!--[if gte mso 6]>
 <style>
