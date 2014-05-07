8768eea72edce08ef0bfca0c1ad261d967dc8301
kentucky <knt92@hotmail.fr>
2014-05-07 23:15:35 +0200

    ajout des prix pour chaque offre + liaison paypal, (non fini)

--- view/pricing.php
+++ view/pricing.php
@@ -17,6 +17,26 @@
     </head>
 <?php
 include '../header/menu.php';
+
+//var_dump($_SESSION['paypal']);
+if(isset($_SESSION['paypal']))
+{
+    $paypal = $_SESSION['paypal'];
+    var_dump($paypal);
+}
+
+/*$item = array(
+    array(
+    "name"  =>  "Compte - Premium",
+    "price"=> 10.0,
+    "count"=> 1
+    ),
+    array(
+        "name"  =>  "Compte - Ultimate",
+        "price"=> 15.0,
+        "count"=> 1
+    )
+);*/
 ?>
 
     <!-- Pricing Option3 -->
@@ -32,26 +52,27 @@ include '../header/menu.php';
                     <div class="plan">
                         <div class="wrapper">
                             <h3>Free</h3>
+
                             <div class="price">
-                                <span class="dollar">$</span> 
-                                <span class="qty">0</span> 
-                                <span class="month">/month</span>
+
+                                <span class="qty">10</span>
+                                <span class="month">Go</span>
                             </div>
                             <div class="features">
                                 <p>
-                                    <strong>10</strong>
-                                    Shared Projects
+                                    <strong>1</strong>
+                                    files / DL
                                 </p>
                                 <p>
-                                    <strong>4</strong>
-                                    Team Members
+                                    <strong>140ko/s</strong>
+                                    bandwich
                                 </p>
                                 <p>
-                                    <strong>10</strong>
-                                    Storage
+                                    <strong>30</strong>
+                                    days of storage
                                 </p>
                             </div>
-                            <a class="order" href="#">ORDER NOW</a>
+
                         </div>
                     </div>
                 </div>
@@ -61,38 +82,76 @@ include '../header/menu.php';
                         <div class="wrapper">
                             <h3>Premium</h3>
                             <div class="price">
-                                <span class="dollar">$</span> 
-                                <span class="qty">65</span> 
+                                <span class="dollar">€</span>
+                                <span class="qty">10</span>
                                 <span class="month">/month</span>
+                            </div>
+                            <div class="price">
+
+                                <span class="qty">250</span>
+                                <span class="month">Go</span>
                             </div>
                             <div class="features">
                                 <p>
-                                    <strong>10</strong>
-                                    Shared Projects
+                                    <strong>Multiple</strong>
+                                    files / DL
                                 </p>
                                 <p>
-                                    <strong>4</strong>
-                                    Team Members
+                                    <strong>1Mb/s</strong>
+                                    bandwich
                                 </p>
                                 <p>
-                                    <strong>10</strong>
-                                    Storage
+                                    <strong>Unlimited</strong>
+                                    days of storage
                                 </p>
                             </div>
-                            <a class="order" href="#">ORDER NOW</a>
+                            <form target="_blank" class="paypal" action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                                <input type="hidden" name="cmd" value="_s-xclick">
+                                <input type="hidden" name="hosted_button_id" value="BX5Q3MK47TUDQ">
+                                <input name="notify_url" type="hidden" value="http://3edfd387.ngrok.com/Cubbyhole/controller/ipnTemplate.php" />
+                                <input name="return" type="hidden" value="http://3edfd387.ngrok.com/Cubbyhole/view/success-pricing.php" />
+                                <input name="cancel_return" type="hidden" value="http://3edfd387.ngrok.com/Cubbyhole/view/pricing.php" />
+                                <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                                <img alt="" border="0" src="https://www.sandbox.paypal.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
+                            </form>
+                            <!--<form target="_blank" action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+                                <input name="amount" type="hidden" value="0.5" />
+                                <input name="currency_code" type="hidden" value="EUR" />
+                                <input name="shipping" type="hidden" value="0.00" />
+                                <input name="tax" type="hidden" value="0.00" />
+                                <input name="return" type="hidden" value="http://314b2647.ngrok.com/Cubbyhole/view/success-pricing.php" />
+                                <input name="cancel_return" type="hidden" value="http://localhost:8081/Cubbyhole/view/pricing.php" />
+                                <input name="notify_url" type="hidden" value="http://314b2647.ngrok.com/Cubbyhole/controller/ipn.php" />
+                                <!--<input name="notify_url" type="hidden" value="http://ksato.fr/controller/ipn.php" />-->
+                                <!--<input name="cmd" type="hidden" value="_xclick" />
+                                <input name="business" type="hidden" value="naindrag@orange.fr" />
+                                <input name="item_name" type="hidden" value="Cubbyhole - Compte Premium" />
+                                <input name="no_note" type="hidden" value="1" />
+                                <input name="lc" type="hidden" value="FR" />
+                                <input name="bn" type="hidden" value="PP-BuyNowBF" />
+                                <input name="custom" type="hidden" value="var1=1&var2=lol" />
+                                <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" >
+                            </form>-->
+
+
                         </div>
                     </div>
                 </div>
                 <!-- Plan Box -->
-                <div class="col-sm-3 ultra">
+                <div class="col-sm-3">
                     <div class="plan">
                         <div class="wrapper">
-                            <img class="ribbon" src="../content/img/badge.png">
+
                             <h3>Ultimate</h3>
                             <div class="price">
-                                <span class="dollar">$</span> 
-                                <span class="qty">99</span> 
+                                <span class="dollar">€</span>
+                                <span class="qty">15</span>
                                 <span class="month">/month</span>
+                            </div>
+                            <div class="price">
+
+                                <span class="qty">500</span>
+                                <span class="month">Go</span>
                             </div>
                             <div class="features">
                                 <p>
@@ -108,18 +167,26 @@ include '../header/menu.php';
                                      Storage
                                 </p>
                                 <p>
-                                    <strong>Plus </strong>
+                                    <strong>Plus</strong>
                                     Phone Support
                                 </p>
                             </div>
-                            <a class="order" href="#">ORDER NOW</a>
+
+                            <!--<form class="paypal" action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_blank">
+                                <input type="hidden" name="cmd" value="_s-xclick">
+                                <input type="hidden" name="hosted_button_id" value="N9X3E9UW4D6RJ">
+                                <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" >
+                                <img alt="" border="0" src="https://www.sandbox.paypal.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
+                            </form>-->
+
                         </div>
                     </div>
                 </div>
                 <!-- Plan Box -->
-                <div class="col-sm-3">
+                <div class="col-sm-3 ultra">
                     <div class="plan">
                         <div class="wrapper">
+                            <img class="ribbon" src="../content/img/badge.png">
                             <h3>Custom</h3>
                             
                             <div class="features">
@@ -145,129 +212,7 @@ include '../header/menu.php';
     </div>
 <br />
 
-    <div id="in_pricing2">
-        <div class="container">
-            <div class="section_header">
-                <h3>Pricing</h3>
-            </div>
 
-            <div class="row charts_wrapp">
-                <!-- Plan Box -->
-                <div class="col-sm-3">
-                    <div class="plan">
-                        <div class="wrapper">
-                            <h3>Free</h3>
-                            <div class="price">
-                                <span class="dollar">$</span>
-                                <span class="qty">0</span>
-                                <span class="month">/month</span>
-                            </div>
-                            <div class="features">
-                                <p>
-                                    <strong>10</strong>
-                                    Shared Projects
-                                </p>
-                                <p>
-                                    <strong>4</strong>
-                                    Team Members
-                                </p>
-                                <p>
-                                    <strong>10</strong>
-                                    Storage
-                                </p>
-                            </div>
-                            <a class="order" href="#">ORDER NOW</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- Plan Box -->
-                <div class="col-sm-3">
-                    <div class="plan">
-                        <div class="wrapper">
-                            <h3>Premium</h3>
-                            <div class="price">
-                                <span class="dollar">$</span>
-                                <span class="qty">65</span>
-                                <span class="month">/month</span>
-                            </div>
-                            <div class="features">
-                                <p>
-                                    <strong>10</strong>
-                                    Shared Projects
-                                </p>
-                                <p>
-                                    <strong>4</strong>
-                                    Team Members
-                                </p>
-                                <p>
-                                    <strong>10</strong>
-                                    Storage
-                                </p>
-                            </div>
-                            <a class="order" href="#">ORDER NOW</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- Plan Box -->
-                <div class="col-sm-3 ultra">
-                    <div class="plan">
-                        <div class="wrapper">
-                            <img class="ribbon" src="../content/img/badge.png">
-                            <h3>Ultimate</h3>
-                            <div class="price">
-                                <span class="dollar">$</span>
-                                <span class="qty">99</span>
-                                <span class="month">/month</span>
-                            </div>
-                            <div class="features">
-                                <p>
-                                    <strong>10</strong>
-                                    Shared Projects
-                                </p>
-                                <p>
-                                    <strong>25</strong>
-                                    Team Members
-                                </p>
-                                <p>
-                                    <strong>Unlimited</strong>
-                                    Storage
-                                </p>
-                                <p>
-                                    <strong>Plus </strong>
-                                    Phone Support
-                                </p>
-                            </div>
-                            <a class="order" href="#">ORDER NOW</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- Plan Box -->
-                <div class="col-sm-3">
-                    <div class="plan">
-                        <div class="wrapper">
-                            <h3>Custom</h3>
-
-                            <div class="features">
-                                <p>
-                                    <strong>10</strong>
-                                    Shared Projects
-                                </p>
-                                <p>
-                                    <strong>4</strong>
-                                    Team Members
-                                </p>
-                                <p>
-                                    <strong>10</strong>
-                                    Storage
-                                </p>
-                            </div>
-                            <a class="order" href="#">ORDER NOW</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 
 
       <?php include '../footer/footer.php'; ?>
