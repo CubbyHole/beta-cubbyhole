579c3a0623ea41efe53a1bd1648f47b668d41e20
kentucky <knt92@hotmail.fr>
2014-05-07 23:16:39 +0200

    modele page de reset password

--- view/reset.php
+++ view/reset.php
@@ -1,136 +1,74 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Cubbyhole - Register</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
-    <!-- Styles -->
-    <link href="css/bootstrap/bootstrap.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="css/compiled/bootstrap-overrides.css" type="text/css" />
-    <link rel="stylesheet" type="text/css" href="css/compiled/theme.css" />
-    
-    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css' />
+<?php
+include'../header/header.php';
+?>
+<!-- Styles -->
+<link rel="stylesheet" href="../content/css/bootstrap/bootstrap.min.css"  />
+<link rel="stylesheet" href="../content/css/compiled/bootstrap-overrides.css" type="text/css" />
+<link rel="stylesheet" href="../content/css/compiled/theme.css" type="text/css" />
+<link rel="stylesheet" href="../content/css/style.css" type="text/css" />
+<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css' />
+<link rel="stylesheet" href="../content/css/compiled/reset.css" type="text/css" media="screen" />
 
-    <link rel="stylesheet" href="css/compiled/reset.css" type="text/css" media="screen" />
-
-    <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
 </head>
+
+
+<?php
+//appel de la barre menu
+include_once '../header/menu.php';
+
+?>
+
 <body>
-    <div class="navbar navbar-inverse navbar-static-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a href="index.php" class="navbar-brand"><strong>CLEAN CANVAS</strong></a>
-            </div>
-
-            <div class="collapse navbar-collapse navbar-ex1-collapse" role="navigation">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="index.php">HOME</a></li>
-                    <li><a href="about-us.php">ABOUT US</a></li>
-                    <li><a href="pricing.php">PRICING</a></li>
-                    <li><a href="contact.php">CONTACT US</a></li>
-                    <li><a href="register.php">REGISTER</a></li>
-                    <li class="active"><a href="login.php">LOGIN</a></li>
-                </ul>
-            </div>
+<div id="reset_pwd" class="reset_page">
+    <?php if(isset($_SESSION['errorMessageReset'])): ?>
+        <div class="alert alert-danger">
+            <p>Message from the server :</p>
+            <br />
+            <?php echo  $_SESSION['errorMessageReset']; ?>
+            <br />
+            <br />
+            <p>Please contact the technical support at <a>technical.support@cubbyhole.com</a> or retry</p>
+            <?php unset($_SESSION['errorMessageReset']); ?>
         </div>
-    </div>
+    <?php endif ?>
 
-    <div id="reset_pwd" class="reset_page">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12 box_wrapper">
-                    <div class="col-md-12">
-                        <div class="box">
-                            <div class="head">
-                                <h4>Enter your email address below to receive instructions on how to reset your password.</h4>
-                                <div class="line"></div>
-                            </div>
-                            <div class="form">
-                                <form>
-                                    <input type="text" placeholder="Email address" class="control-form" />
-                                    <input type="submit" value="Reset password"/>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                    <p class="already">Know your password? <a href="sign-in.html"> Sign in</a></p>
-                </div>
-            </div>
+    <?php if(isset($_SESSION['validMessageReset'])): ?>
+        <div class="alert alert-success">
+            <p>Message from the server :</p>
+            <br />
+            <p>A confirmation email has been sent to your mailbox</p>
+            <?php unset($_SESSION['validMessageReset']); ?>
         </div>
-    </div>
-
-    <!-- starts footer -->
-    <footer id="footer">
-        <div class="container">
-            <div class="row info">
-                <div class="col-sm-6 residence">
-                    <ul>
-                        <li>2301 East Lamar Blvd. Suite 140. City, Arlington.</li>
-                        <li>United States, Zip Code TX 76006.</li>
-                    </ul>
-                </div>
-                <div class="col-sm-5 touch">
-                    <ul>
-                        <li><strong>P.</strong> 1 817 274 2933</li>
-                        <li><strong>E.</strong><a href="#"> bootstrap@twitter.com</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="row credits">
+    <?php endif ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 box_wrapper">
                 <div class="col-md-12">
-                    <div class="row social">
-                        <div class="col-md-12">
-                            <a href="#" class="facebook">
-                                <span class="socialicons ico1"></span>
-                                <span class="socialicons_h ico1h"></span>
-                            </a>
-                            <a href="#" class="twitter">
-                                <span class="socialicons ico2"></span>
-                                <span class="socialicons_h ico2h"></span>
-                            </a>
-                            <a href="#" class="gplus">
-                                <span class="socialicons ico3"></span>
-                                <span class="socialicons_h ico3h"></span>
-                            </a>
-                            <a href="#" class="flickr">
-                                <span class="socialicons ico4"></span>
-                                <span class="socialicons_h ico4h"></span>
-                            </a>
-                            <a href="#" class="pinterest">
-                                <span class="socialicons ico5"></span>
-                                <span class="socialicons_h ico5h"></span>
-                            </a>
-                            <a href="#" class="dribble">
-                                <span class="socialicons ico6"></span>
-                                <span class="socialicons_h ico6h"></span>
-                            </a>
-                            <a href="#" class="behance">
-                                <span class="socialicons ico7"></span>
-                                <span class="socialicons_h ico7h"></span>
-                            </a>
+                    <div class="box">
+                        <div class="head">
+                            <h4>Enter your email address below to receive instructions on how to reset your password.</h4>
+                            <div class="line"></div>
+                        </div>
+                        <div class="form">
+                            <form method="POST" action="/Cubbyhole/controller/reset.php">
+                                <input type="text" name="resetEmail" placeholder="Email address" class="control-form" />
+                                <input type="submit" name="resetPassword" value="SEND"/>
+                            </form>
                         </div>
                     </div>
-                    <div class="row copyright">
-                        <div class="col-md-12">
-                            © 2013 Clean Canvas. All rights reserved. Theme by Detail Canvas.
-                        </div>
-                    </div>
-                </div>            
+                </div>
+
             </div>
         </div>
-    </footer>
+    </div>
+</div>
 
-    <script src="http://code.jquery.com/jquery-latest.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/theme.js"></script>
+<?php
+include '../footer/register.footer.php';
+?>
+
 </body>
 </html>
