b8753f21d420db24f8ea9fcfe5f248f21a2169d7
kentucky <knt92@hotmail.fr>
2014-05-17 01:22:53 +0200

    maj

--- required.php
+++ required.php
@@ -2,18 +2,21 @@
 $projectRoot = $_SERVER['DOCUMENT_ROOT'].'/Cubbyhole';
 
 require_once $projectRoot.'/model/classes/User.class.php';
+require_once $projectRoot.'/model/classes/Payment.class.php';
 require_once $projectRoot.'/model/classes/RefPlan.class.php';
 require_once $projectRoot.'/model/classes/Account.class.php';
 require_once $projectRoot.'/model/classes/RefAction.class.php';
 require_once $projectRoot.'/model/classes/Transaction.class.php';
 
 require_once $projectRoot.'/model/interfaces/AccountManager.interface.php';
+require_once $projectRoot.'/model/interfaces/PaymentManager.interface.php';
 require_once $projectRoot.'/model/interfaces/RefPlanManager.interface.php';
 require_once $projectRoot.'/model/interfaces/UserManager.interface.php';
 require_once $projectRoot.'/model/interfaces/RefActionManager.interface.php';
 require_once $projectRoot.'/model/interfaces/TransactionManager.interface.php';
 
 require_once $projectRoot.'/model/pdo/AbstractPdoManager.class.php';
+require_once $projectRoot.'/model/pdo/PaymentPdoManager.class.php';
 require_once $projectRoot.'/model/pdo/UserPdoManager.class.php';
 require_once $projectRoot.'/model/pdo/RefPlanPdoManager.class.php';
 require_once $projectRoot.'/model/pdo/AccountPdoManager.class.php';
